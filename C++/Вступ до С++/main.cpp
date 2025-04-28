4939d4c68cd0acbd219ec425f335852a13dce0ad
Frontasttttttt <121360768+Frontasttttttt@users.noreply.github.com>
2025-04-28 12:13:47 +0300

    Add files via upload

---
+++ C++/Вступ до С++/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+//EX. 1
+
+// int one()
+// {
+//     cout << "\tVitas - The 7th Element" << endl;
+//     cout << "CHANDRA BRAMBRA CHANDRA CHANDRA BENDRAM..." << endl;
+//     cout << "OOOOOOO, CHANDRA BRAMBRA CHANDRA CHANDRA BENDRAM..." << endl;
+//     cout << "CHANDRA BRAMBRA CHANDRA CHANDRA BENDRAM..." << endl;
+//     cout << "OOOOOOO, CHANDRA BRAMBRA CHANDRA CHANDRA BENDRAM..." << endl;
+//     system("pause");
+// }
+
+//EX. 3
+
+// int three()
+// {
+//     cout << "\tPuppies for sale" << "\n";
+//     cout << "\tDog breed: Gleb" << "\n";
+//     cout << "\tTel: 322-322-322" << "\n";
+//     cout << "\t322\t|322-322|" << "\n";
+//     cout << "\t322\t|322-322|" << "\n";
+//     cout << "\t322\t|322-322|" << "\n";
+//     system("pause");
+// }
